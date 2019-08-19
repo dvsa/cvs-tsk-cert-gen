fb3735c23ecd3a3157261d8a390b58c25290b83d
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-08-19 12:55:15 +0100

    CVSB-3933 HGV/TRL cert template changes.

--- src/services/CertificateGenerationService.ts
+++ src/services/CertificateGenerationService.ts
@@ -102,7 +102,7 @@ class CertificateGenerationService {
                     testTypeName: testResult.testTypes.testTypeName,
                     testTypeResult: testResult.testTypes.testResult,
                     dateOfIssue: moment().format("D MMMM YYYY"),
-                    certificateType: certificateTypes[testType.testResult].split(".")[0],
+                    certificateType: certificateTypes[vehicleTestRes].split(".")[0],
                     fileFormat: "pdf",
                     fileName: `${testResult.testResultId}_${testResult.vin}_${testResult.order.current}.pdf`,
                     fileSize: responseBuffer.byteLength.toString(),
