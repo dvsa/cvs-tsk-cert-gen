bcd9134a2d3102f818e2725d6e750ecbc306656e
Bhavana Siddappa Hemanth <bhemanth@deloitte.co.uk>
2019-09-16 11:42:50 +0100

    Revert "CVSB-7751: Testing PRS defects changes for cert-gen"
    This reverts commit 2b1b223232c59d42778c8273a8f7342f8f232d6d.

--- src/services/CertificateGenerationService.ts
+++ src/services/CertificateGenerationService.ts
@@ -316,22 +316,18 @@ class CertificateGenerationService {
         rawDefects.forEach((defect: any) => {
             switch (defect.deficiencyCategory.toLowerCase()) {
                 case "dangerous":
-                    /*
                     if ((testTypes.testResult === TestResultType.PRS || defect.prs)  && type === "FAIL_DATA") {
                         defects.PRSDefects.push(this.formatDefect(defect));
                     } else if (testTypes.testResult === "fail") {
                         defects.DangerousDefects.push(this.formatDefect(defect));
-                    }*/
-                    defect.prs ? defects.PRSDefects.push(this.formatDefect(defect)): defects.DangerousDefects.push(this.formatDefect(defect));
+                    }
                     break;
                 case "major":
-                    /*
                     if ((testTypes.testResult === TestResultType.PRS || defect.prs) && type === "FAIL_DATA") {
                         defects.PRSDefects.push(this.formatDefect(defect));
                     } else if (testTypes.testResult === "fail") {
                         defects.MajorDefects.push(this.formatDefect(defect));
-                    }*/
-                    defect.prs ? defects.PRSDefects.push(this.formatDefect(defect)): defects.MajorDefects.push(this.formatDefect(defect));
+                    }
                     break;
                 case "minor":
                     defects.MinorDefects.push(this.formatDefect(defect));
