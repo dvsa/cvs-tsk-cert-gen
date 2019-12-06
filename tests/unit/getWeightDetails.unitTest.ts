6f4c960081032691fcfdf3b5e8fdeb6f6c106702
Umer Jaffer <ujaffer@deloitte.co.uk>
2019-12-06 10:12:13 +0000

    extra tests to add more coverage

--- tests/unit/getWeightDetails.unitTest.ts
+++ tests/unit/getWeightDetails.unitTest.ts
@@ -8,6 +8,7 @@ import techRecordsRwt from "../resources/tech-records-response-rwt.json";
 const sandbox = sinon.createSandbox();
 import {cloneDeep} from "lodash";
 import { IWeightDetails, ITestResult } from "../../src/models";
+import { HTTPError } from "../../src/models/HTTPError";
 
 describe("cert-gen", () => {
     const certificateGenerationService: CertificateGenerationService = Injector.resolve<CertificateGenerationService>(CertificateGenerationService, [S3BucketMockService, LambdaMockService]);
@@ -61,6 +62,51 @@ describe("cert-gen", () => {
                  certificateGenerationService.getWeightDetails(testResult).then((weightDetails) => {
                     expect(weightDetails).toEqual(expectedWeightDetails);
                     getTechRecordStub.restore();
+                    });
+                });
+            });
+        });
+    });
+
+        context("CertGenService for Roadworthiness test", () => {
+        context("when a passing test result for Roadworthiness test for HGV is read from the queue", () => {
+        const event: any = cloneDeep(queueEventPass);
+        const testResult: ITestResult = JSON.parse(event.Records[1].body);
+        context("and tech record for vehicle is not found", () => {
+        it("should throw error", () => {
+             const techRecordResponseRwtMock = null;
+
+             const getTechRecordStub = sandbox.stub(CertificateGenerationService.prototype, "getTechRecord").resolves(techRecordResponseRwtMock);
+
+             expect.assertions(1);
+             const expectedError = new HTTPError(500, "No vehicle found for Roadworthiness test certificate!");
+             certificateGenerationService.getWeightDetails(testResult).catch((err) => {
+                expect(err).toEqual(expectedError);
+                getTechRecordStub.restore();
+            });
+                });
+            });
+        });
+    });
+
+        context("CertGenService for Roadworthiness test", () => {
+        context("when a passing test result for Roadworthiness test for TRL is read from the queue", () => {
+            const event: any = cloneDeep(queueEventPass);
+            const testResult: ITestResult = JSON.parse(event.Records[2].body);
+            testResult.testTypes.testTypeId = "91";
+            testResult.vin = "T12768594";
+            testResult.trailerId = "0285678";
+            context("and weightDetails are fetched but not axles array is found", () => {
+            it("it should throw error", () => {
+                 const techRecordResponseRwtMock = cloneDeep(techRecordsRwt[0]);
+                 delete techRecordResponseRwtMock.techRecord[0].axles;
+                 const getTechRecordStub = sandbox.stub(CertificateGenerationService.prototype, "getTechRecord").resolves(techRecordResponseRwtMock);
+
+                 expect.assertions(1);
+                 const expectedError = new HTTPError(500, "No axle weights for Roadworthiness test certificates!");
+                 certificateGenerationService.getWeightDetails(testResult).catch((err) => {
+                expect(err).toEqual(expectedError);
+                getTechRecordStub.restore();
                 });
              });
          });
