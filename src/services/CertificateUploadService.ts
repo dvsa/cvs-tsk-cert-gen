ca7c43a32c1583ba13c71b3bb6e36416de8cf185
JonLazariniDD <62697030+JonLazariniDD@users.noreply.github.com>
2021-08-20 12:32:23 +0100

    Feature/cvsb 19465 (#70)
    * chore(cvsb-19465): update README
    
    chore(cvsb-19465): update service
    
    chore(cvsb-19465): add hook
    
    fix(cvsb-19465): fix
    
    chore(cvsb-19465): flags to detect hanging promises and force kill
    
    chore(cvsb-19465): change coverage cfg and temp comment tests
    
    chore(cvsb-19465): uncomment only CertGen function block
    
    chore(cvsb-19465): increase timeout for cert gen file
    
    * chore(cvsb-19465): rebase and clean deps
    
    * Update sonar-project.properties

--- src/services/CertificateUploadService.ts
+++ src/services/CertificateUploadService.ts
@@ -1,47 +1,53 @@
-import {Service} from "../models/injector/ServiceDecorator";
-import {S3BucketService} from "./S3BucketService";
-import {IGeneratedCertificateResponse} from "../models";
-import {ManagedUpload, Metadata} from "aws-sdk/clients/s3";
+import { Service } from "../models/injector/ServiceDecorator";
+import { S3BucketService } from "./S3BucketService";
+import { IGeneratedCertificateResponse } from "../models";
+import { ManagedUpload, Metadata } from "aws-sdk/clients/s3";
 
 /**
  * Service class for uploading certificates to S3
  */
 @Service()
 class CertificateUploadService {
-    private readonly s3BucketService: S3BucketService;
+  private readonly s3BucketService: S3BucketService;
 
-    constructor(s3BucketService: S3BucketService) {
-        this.s3BucketService = s3BucketService;
+  constructor(s3BucketService: S3BucketService) {
+    this.s3BucketService = s3BucketService;
+  }
+
+  /**
+   * Uploads a generated certificate to S3 bucket
+   * @param payload
+   */
+  public uploadCertificate(
+    payload: IGeneratedCertificateResponse
+  ): Promise<ManagedUpload.SendData> {
+    let shouldEmailCertificate = payload.shouldEmailCertificate;
+
+    if (shouldEmailCertificate !== "false") {
+      shouldEmailCertificate = "true";
     }
 
-    /**
-     * Uploads a generated certificate to S3 bucket
-     * @param payload
-     */
-    public uploadCertificate(payload: IGeneratedCertificateResponse): Promise<ManagedUpload.SendData> {
-        let shouldEmailCertificate = payload.shouldEmailCertificate;
+    const metadata: Metadata = {
+      "vrm": payload.vrm,
+      "test-type-name": payload.testTypeName,
+      "test-type-result": payload.testTypeResult,
+      "date-of-issue": payload.dateOfIssue,
+      "cert-type": payload.certificateType,
+      "file-format": payload.fileFormat,
+      "file-size": payload.fileSize,
+      "cert-index": payload.certificateOrder.current.toString(),
+      "total-certs": payload.certificateOrder.total.toString(),
+      "email": payload.email,
+      "should-email-certificate": shouldEmailCertificate,
+    };
 
-        if (shouldEmailCertificate !== "false") {
-            shouldEmailCertificate = "true";
-        }
-
-        const metadata: Metadata = {
-            "vrm": payload.vrm,
-            "test-type-name": payload.testTypeName,
-            "test-type-result": payload.testTypeResult,
-            "date-of-issue": payload.dateOfIssue,
-            "cert-type": payload.certificateType,
-            "file-format": payload.fileFormat,
-            "file-size": payload.fileSize,
-            "cert-index": payload.certificateOrder.current.toString(),
-            "total-certs": payload.certificateOrder.total.toString(),
-            "email": payload.email,
-            "should-email-certificate": shouldEmailCertificate
-        };
-
-        return this.s3BucketService.upload(`cvs-cert-${process.env.BUCKET}`, payload.fileName, payload.certificate, metadata);
-    }
-
+    return this.s3BucketService.upload(
+      `cvs-cert-${process.env.BUCKET}`,
+      payload.fileName,
+      payload.certificate,
+      metadata
+    );
+  }
 }
 
 export { CertificateUploadService };
--- tests/unit/CertificateGenerationService.unitTest.ts
+++ tests/unit/CertificateGenerationService.unitTest.ts
@@ -1,9 +1,9 @@
-import {CertificateGenerationService} from "../../src/services/CertificateGenerationService";
+import { CertificateGenerationService } from "../../src/services/CertificateGenerationService";
 import sinon from "sinon";
 import techRecordResp from "../resources/tech-records-response.json";
 import techRecordRespHGV from "../resources/tech-records-response-HGV.json";
-import {AWSError, Lambda, Response} from "aws-sdk";
-import {LambdaService} from "../../src/services/LambdaService";
+import { AWSError, Lambda, Response } from "aws-sdk";
+import { LambdaService } from "../../src/services/LambdaService";
 
 describe("Certificate Generation Service", () => {
   const sandbox = sinon.createSandbox();
@@ -14,164 +14,263 @@ describe("Certificate Generation Service", () => {
   describe("getVehicleMakeAndModel function", () => {
     context("when given a systemNumber with matching record", () => {
       it("should return the record & only invoke the LambdaService once", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke").resolves(AWSResolve(JSON.stringify(techRecordResp)));
+        const LambdaStub = sandbox
+          .stub(LambdaService.prototype, "invoke")
+          .resolves(AWSResolve(JSON.stringify(techRecordResp)));
         // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
+        const certGenSvc = new CertificateGenerationService(
+          null as any,
+          new LambdaService(new Lambda())
+        );
         const testResultMock = {
-          systemNumber: "12345678"
+          systemNumber: "12345678",
         };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
+        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+          testResultMock
+        );
         expect(LambdaStub.calledOnce).toBeTruthy();
-        const lambdaArgs = JSON.parse(LambdaStub.firstCall.args[0].Payload as string);
-        expect(lambdaArgs.queryStringParameters.searchCriteria).toEqual("systemNumber");
-        expect(makeAndModel).toEqual({Make: "Mercedes", Model: "632,01"});
+        const lambdaArgs = JSON.parse(
+          LambdaStub.firstCall.args[0].Payload as string
+        );
+        expect(lambdaArgs.queryStringParameters.searchCriteria).toEqual(
+          "systemNumber"
+        );
+        expect(makeAndModel).toEqual({ Make: "Mercedes", Model: "632,01" });
       });
     });
 
-    context("when given a systemNumber  with no matching record and a vin with matching record", () => {
-      it("should return the record & invoke the LambdaService twice", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke")
-          .onFirstCall().resolves(AWSReject("no"))
-          .onSecondCall().resolves(AWSResolve(JSON.stringify(techRecordResp)));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
-        const testResultMock = {
-          systemNumber: "134567889",
-          vin: "abc123"
-        };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(LambdaStub.calledTwice).toBeTruthy();
-        const lambdaArgs = JSON.parse(LambdaStub.secondCall.args[0].Payload as string);
-        expect(lambdaArgs.queryStringParameters.searchCriteria).toEqual("all");
-        expect(makeAndModel).toEqual({Make: "Mercedes", Model: "632,01"});
-      });
-    });
+    context(
+      "when given a systemNumber  with no matching record and a vin with matching record",
+      () => {
+        it("should return the record & invoke the LambdaService twice", async () => {
+          const LambdaStub = sandbox
+            .stub(LambdaService.prototype, "invoke")
+            .onFirstCall()
+            .resolves(AWSReject("no"))
+            .onSecondCall()
+            .resolves(AWSResolve(JSON.stringify(techRecordResp)));
+          // @ts-ignore
+          const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+          );
+          const testResultMock = {
+            systemNumber: "134567889",
+            vin: "abc123",
+          };
+          const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+            testResultMock
+          );
+          expect(LambdaStub.calledTwice).toBeTruthy();
+          const lambdaArgs = JSON.parse(
+            LambdaStub.secondCall.args[0].Payload as string
+          );
+          expect(lambdaArgs.queryStringParameters.searchCriteria).toEqual(
+            "all"
+          );
+          expect(makeAndModel).toEqual({ Make: "Mercedes", Model: "632,01" });
+        });
+      }
+    );
 
-    context("when given a vin with no matching record but a matching partialVin", () => {
-      it("should return the record & invoke the LambdaService twice", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke")
-          .onFirstCall().resolves(AWSReject("no"))
-          .onSecondCall().resolves(AWSResolve(JSON.stringify(techRecordResp)));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
-        const testResultMock = {
-          vin: "abc123",
-          partialVin: "abc123"
-        };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(LambdaStub.calledOnce).toBeFalsy();
-        expect(LambdaStub.calledTwice).toBeTruthy();
-        expect(makeAndModel).toEqual({Make: "Mercedes", Model: "632,01"});
-      });
-    });
+    context(
+      "when given a vin with no matching record but a matching partialVin",
+      () => {
+        it("should return the record & invoke the LambdaService twice", async () => {
+          const LambdaStub = sandbox
+            .stub(LambdaService.prototype, "invoke")
+            .onFirstCall()
+            .resolves(AWSReject("no"))
+            .onSecondCall()
+            .resolves(AWSResolve(JSON.stringify(techRecordResp)));
+          // @ts-ignore
+          const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+          );
+          const testResultMock = {
+            vin: "abc123",
+            partialVin: "abc123",
+          };
+          const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+            testResultMock
+          );
+          expect(LambdaStub.calledOnce).toBeFalsy();
+          expect(LambdaStub.calledTwice).toBeTruthy();
+          expect(makeAndModel).toEqual({ Make: "Mercedes", Model: "632,01" });
+        });
+      }
+    );
 
-    context("when given a vin and partialVin with no matching record but a matching VRM", () => {
-      it("should return the record & invoke the LambdaService three times", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke")
-          .onFirstCall().resolves(AWSReject("no"))
-          .onSecondCall().resolves(AWSReject("no"))
-          .onThirdCall().resolves(AWSResolve(JSON.stringify(techRecordResp)));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
-        const testResultMock = {
-          vin: "abc123",
-          partialVin: "abc123",
-          vrm: "testvrm"
-        };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(LambdaStub.calledOnce).toBeFalsy();
-        expect(LambdaStub.calledTwice).toBeFalsy();
-        expect(LambdaStub.calledThrice).toBeTruthy();
-        expect(makeAndModel).toEqual({Make: "Mercedes", Model: "632,01"});
-      });
-    });
+    context(
+      "when given a vin and partialVin with no matching record but a matching VRM",
+      () => {
+        it("should return the record & invoke the LambdaService three times", async () => {
+          const LambdaStub = sandbox
+            .stub(LambdaService.prototype, "invoke")
+            .onFirstCall()
+            .resolves(AWSReject("no"))
+            .onSecondCall()
+            .resolves(AWSReject("no"))
+            .onThirdCall()
+            .resolves(AWSResolve(JSON.stringify(techRecordResp)));
+          // @ts-ignore
+          const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+          );
+          const testResultMock = {
+            vin: "abc123",
+            partialVin: "abc123",
+            vrm: "testvrm",
+          };
+          const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+            testResultMock
+          );
+          expect(LambdaStub.calledOnce).toBeFalsy();
+          expect(LambdaStub.calledTwice).toBeFalsy();
+          expect(LambdaStub.calledThrice).toBeTruthy();
+          expect(makeAndModel).toEqual({ Make: "Mercedes", Model: "632,01" });
+        });
+      }
+    );
 
-    context("when given a vin, partialVin and VRM with no matching record but a matching TrailerID", () => {
-      it("should return the record & invoke the LambdaService four times", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke")
-          .onFirstCall().resolves(AWSReject("no"))
-          .onSecondCall().resolves(AWSReject("no"))
-          .onThirdCall().resolves(AWSReject("no"))
-          .onCall(3).resolves(AWSResolve(JSON.stringify(techRecordResp)));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
-        const testResultMock = {
-          vin: "abc123",
-          partialVin: "abc123",
-          vrm: "testvrm",
-          trailerId: "testTrailerId"
-        };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(LambdaStub.callCount).toEqual(4);
-        expect(makeAndModel).toEqual({Make: "Mercedes", Model: "632,01"});
-      });
-    });
+    context(
+      "when given a vin, partialVin and VRM with no matching record but a matching TrailerID",
+      () => {
+        it("should return the record & invoke the LambdaService four times", async () => {
+          const LambdaStub = sandbox
+            .stub(LambdaService.prototype, "invoke")
+            .onFirstCall()
+            .resolves(AWSReject("no"))
+            .onSecondCall()
+            .resolves(AWSReject("no"))
+            .onThirdCall()
+            .resolves(AWSReject("no"))
+            .onCall(3)
+            .resolves(AWSResolve(JSON.stringify(techRecordResp)));
+          // @ts-ignore
+          const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+          );
+          const testResultMock = {
+            vin: "abc123",
+            partialVin: "abc123",
+            vrm: "testvrm",
+            trailerId: "testTrailerId",
+          };
+          const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+            testResultMock
+          );
+          expect(LambdaStub.callCount).toEqual(4);
+          expect(makeAndModel).toEqual({ Make: "Mercedes", Model: "632,01" });
+        });
+      }
+    );
 
     context("when given a vehicle details with no matching records", () => {
       it("should call Tech Records Lambda 4 times and then throw an error", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke").resolves(AWSReject("no"));
+        const LambdaStub = sandbox
+          .stub(LambdaService.prototype, "invoke")
+          .resolves(AWSReject("no"));
         // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
+        const certGenSvc = new CertificateGenerationService(
+          null as any,
+          new LambdaService(new Lambda())
+        );
         const testResultMock = {
           vin: "abc123",
           partialVin: "abc123",
           vrm: "testvrm",
-          trailerId: "testTrailerId"
+          trailerId: "testTrailerId",
         };
         try {
           await certGenSvc.getVehicleMakeAndModel(testResultMock);
         } catch (e) {
           expect(LambdaStub.callCount).toEqual(4);
           expect(e).toBeInstanceOf(Error);
-          expect(e.message).toEqual("Unable to retrieve unique Tech Record for Test Result");
+          expect(e.message).toEqual(
+            "Unable to retrieve unique Tech Record for Test Result"
+          );
         }
       });
     });
 
-    context("when given a vehicle details with missing vehicle detail fields and no match", () => {
-      it("should call Tech Records Lambda matching number (2) times and then throw an error", async () => {
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke").resolves(AWSReject("no"));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
-        const testResultMock = {
-          vin: "abc123",
-          trailerId: "testTrailerId"
-        };
-        try {
-          await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        } catch (e) {
-          expect(LambdaStub.callCount).toEqual(2);
-          expect(e).toBeInstanceOf(Error);
-          expect(e.message).toEqual("Unable to retrieve unique Tech Record for Test Result");
-        }
-      });
-    });
+    context(
+      "when given a vehicle details with missing vehicle detail fields and no match",
+      () => {
+        it("should call Tech Records Lambda matching number (2) times and then throw an error", async () => {
+          const LambdaStub = sandbox
+            .stub(LambdaService.prototype, "invoke")
+            .resolves(AWSReject("no"));
+          // @ts-ignore
+          const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+          );
+          const testResultMock = {
+            vin: "abc123",
+            trailerId: "testTrailerId",
+          };
+          try {
+            await certGenSvc.getVehicleMakeAndModel(testResultMock);
+          } catch (e) {
+            expect(LambdaStub.callCount).toEqual(2);
+            expect(e).toBeInstanceOf(Error);
+            expect(e.message).toEqual(
+              "Unable to retrieve unique Tech Record for Test Result"
+            );
+          }
+        });
+      }
+    );
 
     context("when lookup returns a PSV tech record", () => {
       it("should return make and model from chassis details", async () => {
         const techRecord = JSON.parse(techRecordResp.body);
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke").resolves(AWSResolve(JSON.stringify(techRecordResp)));
+        const LambdaStub = sandbox
+          .stub(LambdaService.prototype, "invoke")
+          .resolves(AWSResolve(JSON.stringify(techRecordResp)));
         // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
+        const certGenSvc = new CertificateGenerationService(
+          null as any,
+          new LambdaService(new Lambda())
+        );
         const testResultMock = {
-          systemNumber: "12345678"
+          systemNumber: "12345678",
         };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(makeAndModel).toEqual({Make: techRecord[0].techRecord[0].chassisMake, Model: techRecord[0].techRecord[0].chassisModel});
+        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+          testResultMock
+        );
+        expect(makeAndModel).toEqual({
+          Make: techRecord[0].techRecord[0].chassisMake,
+          Model: techRecord[0].techRecord[0].chassisModel,
+        });
       });
     });
 
     context("when lookup returns a non-PSV tech record", () => {
       it("should return make and model from not-chassis details", async () => {
         const techRecord = JSON.parse(techRecordRespHGV.body);
-        const LambdaStub = sandbox.stub(LambdaService.prototype, "invoke").resolves(AWSResolve(JSON.stringify(techRecordRespHGV)));
-        // @ts-ignore
-        const certGenSvc = new CertificateGenerationService(null, new LambdaService(new Lambda()));
+        const LambdaStub = sandbox
+          .stub(LambdaService.prototype, "invoke")
+          .resolves(AWSResolve(JSON.stringify(techRecordRespHGV)));
+        const certGenSvc = new CertificateGenerationService(
+          // @ts-ignore
+          null,
+          new LambdaService(new Lambda())
+        );
         const testResultMock = {
-          systemNumber: "12345678"
+          systemNumber: "12345678",
         };
-        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(testResultMock);
-        expect(makeAndModel).toEqual({Make: techRecord[0].techRecord[0].make, Model: techRecord[0].techRecord[0].model});
+        const makeAndModel = await certGenSvc.getVehicleMakeAndModel(
+          testResultMock
+        );
+        expect(makeAndModel).toEqual({
+          Make: techRecord[0].techRecord[0].make,
+          Model: techRecord[0].techRecord[0].model,
+        });
       });
     });
   });
@@ -182,14 +281,14 @@ const AWSResolve = (payload: any) => {
   Object.assign(response, {
     data: {
       StatusCode: 200,
-      Payload: payload
-    }
+      Payload: payload,
+    },
   });
 
   return {
     $response: response,
     StatusCode: 200,
-    Payload: payload
+    Payload: payload,
   };
 };
 
@@ -198,13 +297,13 @@ const AWSReject = (payload: any) => {
   Object.assign(response, {
     data: {
       StatusCode: 400,
-      Payload: payload
-    }
+      Payload: payload,
+    },
   });
 
   return {
     $response: response,
     StatusCode: 400,
-    Payload: payload
+    Payload: payload,
   };
 };
