7213e8263da4ae5f057498319f018e86d699e7e4
Craig Broady <Craig.Broady@dvsa.gov.uk>
2023-05-11 13:01:45 +0100

    updated

--- src/services/CertificateGenerationService.ts
+++ src/services/CertificateGenerationService.ts
@@ -274,7 +274,7 @@ class CertificateGenerationService {
       //
       // TODO RETRIEVE WELSH DEFECTS - API CALL / S3 BUCKET PICKUP & ADD TO PAYLOAD FAIL DATA
       //
-      const welshDefects = { defect: "mock for welsh defects"};
+      const welshDefects = { DefectMock: "mock for welsh defects"};
       //
       if (testTypes.testResult !== TEST_RESULTS.FAIL) {
         const passData = await this.generateCertificateData(
