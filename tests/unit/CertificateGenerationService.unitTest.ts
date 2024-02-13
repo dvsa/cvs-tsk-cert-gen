e8b80902f190f3684d173a0e7888477909d7f7f2
Craig Broady <79261988+cb-cs@users.noreply.github.com>
2024-02-13 10:27:32 +0000

    CB2-9866: IVA defects can be provided to doc-gen from a test (#138)
    * feat(cb2-9866): implemented new logic for IVA30
    
    feat(cb2-9866): simplified logic and added logger
    
    feat(cb2-9866): corrected typo and added more loggers
    
    feat(cb2-9866): renamed for consistency
    
    feat(cb2-9866): more loggers
    
    feat(cb2-9866): updated config.yml
    
    cb2-9866): cleaned up code
    
    feat(cb2-9866): added unit tests
    
    feat(cb2-9866): added name for ivaDefects array
    
    feat(cb2-9866): separated make and model on payload to doc-gen
    
    feat(cb2-9866): added new tests
    
    feat(cb2-9866): added more tests for coverage
    
    feat(cb2-9866): cleaned up test data
    
    feat(cb2-9866): added additional tests for coverage
    
    * feat(cb2-9866): updated logic to use testStartTimestamp instead of createdAt as per PO comments
    
    * feat(cb2-9866): updated to include bodyType model as per test-result-service changes

--- tests/unit/CertificateGenerationService.unitTest.ts
+++ tests/unit/CertificateGenerationService.unitTest.ts
@@ -16,6 +16,7 @@ import techRecordsRwtHgvSearch from "../resources/tech-records-response-rwt-hgv-
 import techRecordsPsv from "../resources/tech-records-response-PSV.json";
 import techRecordsSearchPsv from "../resources/tech-records-response-search-PSV.json";
 import mockTestResult from "../resources/test-result-with-defect.json";
+import mockIvaTestResult from "../resources/test-result-with-iva-defect.json";
 import defectsMock from "../../tests/resources/defects_mock.json";
 import flatDefectsMock from "../../tests/resources/flattened-defects.json";
 import testStationsMock from "../../tests/resources/testStationsMock.json";
@@ -1112,6 +1113,37 @@ describe("Certificate Generation Service", () => {
           logSpy.mockClear();
           jest.resetAllMocks();
         });
+      });
+    });
+  });
+
+  describe("iva 30 logic", () => {
+    context("test isBasicIvaTest logic", () => {
+      it("should return true if test type id on test result exists in basic array", async () => {
+        const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+        );
+
+        const ivaTestResult = cloneDeep(mockIvaTestResult);
+
+        const result: boolean = certGenSvc.isBasicIvaTest(ivaTestResult.testTypes[0].testTypeId);
+
+        expect(result).toBeTruthy();
+      });
+      it("should return false if test type id on test result does not exist in basic array", async () => {
+        const certGenSvc = new CertificateGenerationService(
+            null as any,
+            new LambdaService(new Lambda())
+        );
+
+        const ivaTestResult = cloneDeep(mockIvaTestResult);
+        ivaTestResult.testTypes[0].testTypeId = "130";
+        ivaTestResult.testTypes[0].testTypeName = "Mutual recognition/ end of series & inspection";
+
+        const result: boolean = certGenSvc.isBasicIvaTest(ivaTestResult.testTypes[0].testTypeId);
+
+        expect(result).toBeFalsy();
       });
     });
   });
